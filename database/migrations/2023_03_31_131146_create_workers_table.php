57f4ada7d77723bf097adff23c3796bde833342a
Zeina M Nada <zeinamnada@gmail.com>
2023-03-31 15:56:54 +0200

    init commit

---
+++ database/migrations/2023_03_31_131146_create_workers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->foreign('employer_id')->references('id')->on('employers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->dropForeign(['employer_id']);
+        Schema::dropIfExists('workers');
+    }
+};
