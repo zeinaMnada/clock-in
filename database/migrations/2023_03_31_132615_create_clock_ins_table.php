9e02e27cf3afaf3c9ab60e0e326bf14e773a91f6
Zeina M Nada <zeinamnada@gmail.com>
2023-03-31 15:56:54 +0200

    init commit

---
+++ database/migrations/2023_03_31_132615_create_clock_ins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clock_ins', function (Blueprint $table) {
+            $table->id();
+            table->dateTime('signed_in_at');
+            $table->ipAddress('location');
+            $table->foreign('worker_id')->references('id')->on('workers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->dropForeign(['worker_id']);
+        Schema::dropIfExists('clock_ins');
+    }
+};
